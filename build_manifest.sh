dc7b11d7918d241dab51808e3c53b4d39f94d20d
Fred Gleason <fredg@paravelsystems.com>
2014-09-09 21:08:55 -0400

    2014-09-09 Fred Gleason <fredg@paravelsystems.com> 	* Added 'isolinux/ks/rivendell-bios.cfg'. 	* Added 'isolinux/ks/rivendell-combo.cfg'.

---
+++ build_manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# build_manifest.sh
+#
+# Build the package manifest for Broadcast Appliance
+#
+# (C) Copyright 2014 Fred Gleason <fredg@paravelsystems.com>
+#
+
+ls isolinux/Packages/* > MANIFEST
---
+++ check_manifest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# check_manifest.sh
+#
+# Check the current state of the package repository against the manifest
+#
+# (C) Copyright 2014 Fred Gleason <fredg@paravelsystems.com>
+#
+
+ls isolinux/Packages/* > MANIFEST.tmp
+diff -u --recursive --new-file MANIFEST MANIFEST.tmp > MANIFEST.diff
+
+if test -f MANIFEST.diff -a ! -s MANIFEST.diff ; then
+  rm -f MANIFEST.diff MANIFEST.tmp
+  exit 0
+fi
+
+echo "Package repository does not match manifest!"
+echo
+cat MANIFEST.diff
+rm -f MANIFEST.diff MANIFEST.tmp
+echo
+
+exit 256
+
